20a3ea985c639e62449761918b0682b2d6f02880
dev-rome <haynesjerome@gmail.com>
2022-12-13 18:52:34 -0500

    added theme file for light and dark mode

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  body: "#f6f8ff",
+  text: "#4b6a9b",
+  logo: "#222731"
+};
+
+export const darkTheme = {
+  body: "#141d2f",
+  text: "#ffffff",
+  logo: "#ffffff"
+};
