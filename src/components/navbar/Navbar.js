6c72c534e525298675e020ff6d3b6d31e5edaf08
dev-rome <haynesjerome@gmail.com>
2022-12-13 18:56:05 -0500

    added navbar with button to change between light and dark mode

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import NavbarMoon from "./NavbarMoon";
+import NavbarSun from "./NavbarSun";
+
+const Nav = styled.nav`
+  padding: 1.938rem 1.5rem 0;
+  margin-bottom: 2.25rem;
+`;
+
+const NavWrapper = styled.div`
+  max-width: 45.625rem;
+  margin-inline: auto;
+  display: flex;
+  justify-content: space-between;
+
+  p {
+    font-size: 1.625rem;
+    font-weight: 700;
+    color: ${(props) => props.theme.logo};
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+
+const Navbar = ({ onClick, isLight }) => {
+  return (
+    <Nav>
+      <NavWrapper>
+        <p>devfinder</p>
+        <Button onClick={onClick}>
+          {isLight === "light" ? <NavbarMoon /> : <NavbarSun />}
+        </Button>
+      </NavWrapper>
+    </Nav>
+  );
+};
+
+export default Navbar;
