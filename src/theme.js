a91cff3939251c6ffee09fbc712e97e2c45f2490
dev-rome <haynesjerome@gmail.com>
2022-12-14 15:10:34 -0500

    added colors for logo, form background, and form input

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,15 @@
 export const lightTheme = {
   body: "#f6f8ff",
   text: "#4b6a9b",
-  logo: "#222731"
+  logo: "#222731",
+  formBG: "#ffffff",
+  formInput: "#222731",
 };
 
 export const darkTheme = {
   body: "#141d2f",
   text: "#ffffff",
-  logo: "#ffffff"
+  logo: "#ffffff",
+  formBG: "#1e2a47",
+  formInput: "#ffffff",
 };
